803ba1089e5bfa623b7737fbce944e10296f05de
Shahari93 <shaharilani93@gmail.com>
2020-03-07 23:36:18 +0200

    Timer for button
    Now at the press of the switch weapons button a timer start to count, the color of the button change to gray (start as blue. all colors are placeholders). when the timer is counting the player can't change to the other weapon

--- Zombero/Assets/Scripts/Player/Bullet.cs
+++ Zombero/Assets/Scripts/Player/Bullet.cs
@@ -11,19 +11,19 @@ public class Bullet : MonoBehaviour
     {
         if(floatingJoystick.Vertical==0 && floatingJoystick.Horizontal==0)
         {
-            GameObject b = BulletPool.singleton.Get("PlayerBullet");
+            GameObject bullet = BulletPool.singleton.Get("PlayerBullet");
             if(Random.Range(0,101)<5)
             {
-                if(b!=null&& weaponButton.rangeGO.activeInHierarchy) // check the state of the weapon switch button. if active, player shoot. if not player stop shooting and use sword
+                if(bullet!=null&& weaponButton.rangeGO.activeInHierarchy) // check the state of the weapon switch button. if active, player shoot. if not player stop shooting and use sword
                 {
-                    b.transform.position = this.transform.position;
-                    b.transform.eulerAngles = this.transform.parent.eulerAngles; // takes to rotation angle of the parent object
-                    b.SetActive(true);
+                    bullet.transform.position = this.transform.position;
+                    bullet.transform.eulerAngles = this.transform.parent.eulerAngles; // takes to rotation angle of the parent object
+                    bullet.SetActive(true);
                 }
 
                else if(!weaponButton.rangeGO.activeInHierarchy)
                 {
-                    b.SetActive(false);
+                    bullet.SetActive(false);
                 }
 
             }
--- Zombero/Assets/Scripts/Player/SwitchWeaponsButton.cs
+++ Zombero/Assets/Scripts/Player/SwitchWeaponsButton.cs
@@ -5,10 +5,33 @@ using UnityEngine.UI;
 
 public class SwitchWeaponsButton : MonoBehaviour
 {
-     public GameObject rangeGO = null, meleeGO = null, swordGO = null;
+    public GameObject rangeGO = null, meleeGO = null, swordGO = null;
+    [SerializeField]float timer;
+    [SerializeField]bool isTimerActive = false;
+
     private void Awake()
     {
         swordGO.SetActive(false);
+    }
+
+    private void Update()
+    {
+        TimerForButton();
+    }
+
+    void Timer() // method for a timer to count backwards from X to 0
+    {
+        if (isTimerActive)
+        {
+            timer -= Time.deltaTime;
+            if(timer<=0)
+            {
+                timer = 0;
+                isTimerActive = false;
+                this.gameObject.GetComponent<Button>().enabled = true; // sets the button to true
+                this.gameObject.GetComponent<Image>().color = Color.blue; // set the button color to blue
+            }
+        }
     }
 
     public void OnWeaponSwitchButtonPressed()
@@ -18,12 +41,40 @@ public class SwitchWeaponsButton : MonoBehaviour
             rangeGO.SetActive(false);
             meleeGO.SetActive(true);
             swordGO.SetActive(true);
+            SetColor();
         }
         else if(meleeGO.activeInHierarchy)
         {
             rangeGO.SetActive(true);
             meleeGO.SetActive(false);
             swordGO.SetActive(false);
+            SetColor();
         }
+    }
+
+    void TimerForButton()
+    {
+        if(rangeGO.activeInHierarchy)
+        {
+            ActivateTimer();
+        }
+
+        else if (meleeGO.activeInHierarchy)
+        {
+            ActivateTimer();
+        }
+    }
+
+    void ActivateTimer()
+    {
+        isTimerActive = true;
+        this.gameObject.GetComponent<Button>().enabled = false;
+        Timer();
+    }
+
+    void SetColor()
+    {
+        this.gameObject.GetComponent<Image>().color = Color.gray;
+        timer = 5f;
     }
 }
