39d345f418ed442b661e1724154e8fc762e37a92
Shahari93 <shaharilani93@gmail.com>
2020-03-07 21:28:39 +0200

    Health bar

---
+++ Zombero/Assets/Scripts/inheritance Classes/Player&NPC/HealthBar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour 
+{
+    public int _health = 100;
+
+    public void PlayerDamageDealt(int _howMuchDamage) // can be used for the player and npc
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _health = _health - _howMuchDamage;
+        }
+    }
+}
