d85a4799399480f2221835874896b6899d8eaf7d
Shahari93 <shaharilani93@gmail.com>
2020-04-15 18:21:32 +0300

    Bullet time fix
    Now the bullet shoots every fixed time

--- Zombero/Assets/Scripts/Misc/Bullet.cs
+++ Zombero/Assets/Scripts/Misc/Bullet.cs
@@ -4,28 +4,37 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-
+    [SerializeField] private float timer = 2f;
+    private static float currentTimer;
     [SerializeField] FloatingJoystick floatingJoystick = null;
     [SerializeField] SwitchWeaponsButton weaponButton = null;
+
+    private void Awake()
+    {
+        currentTimer = timer;
+    }
+
     private void Update()
     {
-        if(floatingJoystick.Vertical==0 && floatingJoystick.Horizontal==0)
+        currentTimer -= Time.deltaTime;
+        PlayerShoot();
+    }
+
+    private void PlayerShoot()
+    {
+        if (floatingJoystick.Vertical == 0 && floatingJoystick.Horizontal == 0)
         {
             GameObject bullet = BulletPool.singleton.Get("PlayerBullet");
-            if(Random.Range(0,151)<2)
+            if (currentTimer<=0)
             {
                 if (bullet != null && weaponButton.rangeGO.activeInHierarchy) // check the state of the weapon switch button. if active, player shoot. if not player stop shooting and use sword
                 {
                     bullet.transform.position = this.transform.position;
                     bullet.transform.eulerAngles = this.transform.parent.eulerAngles; // takes to rotation angle of the parent object
                     bullet.SetActive(true);
+                    currentTimer = timer;
                 }
-                if(GameManager.Singleton.Enemies.Count<= 0) // stop shooting if there are no enemies on screen
-                {
-                    bullet.SetActive(false);
-                }
-
-                else if(!weaponButton.rangeGO.activeInHierarchy) // stop shooting if the range weapon is not selected
+                if (GameManager.Singleton.Enemies.Count <= 0 || !weaponButton.rangeGO.activeInHierarchy) // stop shooting if there are no enemies on screen
                 {
                     bullet.SetActive(false);
                 }
